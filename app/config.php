f56e189eb5d92cfb40c0df0514063fd6f8c13ea5
app <app@192.168.1.4>
2026-05-15 23:58:21 +0500

    solar reroof page

--- app/config.php
+++ app/config.php
@@ -4,11 +4,14 @@
  */
 
 // Define the base path for URL rewriting and asset loading
+$uri = $_SERVER['REQUEST_URI'];
 if ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['SERVER_ADDR'] === '127.0.0.1') {
-    // Local development (XAMPP)
     define('BASE_PATH', '/fort-energy/public');
+} elseif (strpos($uri, '/staging/public') === 0) {
+    define('BASE_PATH', '/staging/public');
+} elseif (strpos($uri, '/staging') === 0) {
+    define('BASE_PATH', '/staging');
 } else {
-    // Production (adjust if your live site is in a subdirectory)
     define('BASE_PATH', '');
 }
 
